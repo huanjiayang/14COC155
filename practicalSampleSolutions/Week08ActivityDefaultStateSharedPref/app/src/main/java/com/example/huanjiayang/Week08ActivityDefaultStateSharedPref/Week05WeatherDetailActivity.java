ac085c033ddfeba6be2dee738c732b8a2b20f97a
huanjiayang <huanjiayang@hotmail.com>
2014-11-20 09:38:12 +0000

    add example answer for week 08 practical session exercise 2

---
+++ practicalSampleSolutions/Week08ActivityDefaultStateSharedPref/app/src/main/java/com/example/huanjiayang/Week08ActivityDefaultStateSharedPref/Week05WeatherDetailActivity.java
@@ -0,0 +1,50 @@
+package com.example.huanjiayang.Week08ActivityDefaultStateSharedPref;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+
+public class Week05WeatherDetailActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_w05_detail_weather);
+
+        Intent theIntent = getIntent();
+        String theExtra = "";
+
+        if(theIntent.hasExtra("weatherdetail")){
+            theExtra = theIntent.getStringExtra("weatherdetail");
+
+        }else{
+            theExtra = "no data found in extra";
+        }
+
+        ((TextView)findViewById(R.id.weather_detail_text)).setText(theExtra);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.w05_detail_weather, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
